3aaf28a3dabac20d658c036cc72ff7452d0c63d9
TonyBotongChu <zbt945263008@buaa.edu.cn>
2016-12-06 14:06:30 +0800

    setting up the main window

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # typingCode
+This is one of our Java assignments. It is designed to provide typing practice for programmers.
---
+++ src/application/Main.java
@@ -0,0 +1,71 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("demo");
+
+		initRootLayout();
+		
+		showMainSubWin();
+	}
+	
+	public void initRootLayout()
+	{
+		try
+		{
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void showMainSubWin()
+	{
+		try {
+	        // Load person overview.
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(Main.class.getResource("view/MainSubWin.fxml"));
+	        AnchorPane MainSubWin = (AnchorPane) loader.load();
+
+	        // Set person overview into the center of root layout.
+	        rootLayout.setCenter(MainSubWin);
+
+	        // Give the controller access to the main app.
+	       //MainSubWinController controller = loader.getController();
+	        //controller.setMainApp(this);
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
